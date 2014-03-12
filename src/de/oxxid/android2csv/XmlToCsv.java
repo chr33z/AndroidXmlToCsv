d9feef2868d84a4fdea38dab12b875ff1cf96ce7
chr33z <chr33z@googlemail.com>
2014-03-12 22:54:34 +0100

    simplified xml to csv script

--- src/de/oxxid/android2csv/XmlToCsv.java
+++ src/de/oxxid/android2csv/XmlToCsv.java
@@ -19,42 +19,62 @@ public class XmlToCsv {
 	
 	static final char DEL = '\t';
 	
-	public static void dataToCsv(String pathDirectoryXml, String fileName,
-			String origLanguage, String[] targetLanguages) throws IOException{
+	public static void dataToCsv(
+			String pathProjectDirectory,
+			String pathCsvFile,
+			String origLanguage,
+			String[] targetLanguages) throws IOException{
+		
+		// create csv file and add a header to the file
+		File csvFile = createCsvFile(pathCsvFile);
+		if(csvFile != null){
+			System.out.println("Created csv file at " + csvFile);
+		} else {
+			System.out.println("Couldn't create file: " + pathCsvFile);
+			return;
+		}
+		
+		// get android resource folder
+		File androidResourceDirectory = findResourceFolder(new File(pathProjectDirectory), 3, 0);
+		if(androidResourceDirectory != null){
+			System.out.println("Found resource at " + androidResourceDirectory);
+		} else {
+			System.out.println("Couldn't find android resource folder in: " + new File(pathProjectDirectory));
+			return;
+		}
+		
+		// initialize csv writer
+		CSVWriter writer = new CSVWriter(new FileWriter(csvFile), DEL);
+		addHeaderToCsv(writer, origLanguage, targetLanguages);
 		
 		ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
 		int coloumns = targetLanguages.length + 3;
 
-		// create csv file and add a header to the file
-		File csvFile = createCsvDirectory(pathDirectoryXml, fileName);
-		CSVWriter writer = new CSVWriter(new FileWriter(csvFile), DEL);
-		addHeaderToCsv(writer, origLanguage, targetLanguages);
-
-		File sourcePath = new File(pathDirectoryXml);
-		File[] files = sourcePath.listFiles();
-		
-		// iterate all directoryies in source directory
+		// get folders in resource folder and iterate over them
+		File[] files = androidResourceDirectory.listFiles();
 		for (File file : files) {
-			if(file.isDirectory() && equalsSpecifiedLanguage(file.getName(), origLanguage, targetLanguages)){
+			if(file.isDirectory() && isLanguageFolder(file.getName(), origLanguage, targetLanguages)){
+				
+				// get all xml files from that directory
 				ArrayList<File> xmlFiles = Utils.getXmlFilesFromDirectory(file);
+				System.out.println("Found "+xmlFiles.size()+" files in directory \""+ file +"\". Crunching through...");
 
-				System.out.println("Found "+xmlFiles.size()+" files in directory \""+ file.getAbsolutePath() +"\". Crunching through...");
-
-				// store lines for each language (from all language files) in one table
+				// store lines for each language (from all xml files) in one table
 				ArrayList<ArrayList<String>> langTable = new ArrayList<ArrayList<String>>();
 
 				// iterate all xml files in locale directory
 				for (File xmlFile : xmlFiles) {
-					if(xmlFile.isDirectory()) continue;
-
+					if(xmlFile.isDirectory()){
+						continue;
+					}
 					try {
 						Builder parser = new Builder();
 						Document doc = parser.build(xmlFile);
 						Element root = doc.getRootElement();
 
-						// get file contents and store in separate table (one table per xml file)
+						// get file contents and store in separate table (one table for each xml file)
 						ArrayList<ArrayList<String>> xmlTable = new ArrayList<ArrayList<String>>();
-						readXmlStrings(root, xmlTable, coloumns, xmlFile.getName());
+						readXmlFile(root, xmlTable, coloumns, xmlFile.getName());
 
 						// add file table to language table
 						langTable.addAll(xmlTable);
@@ -67,19 +87,45 @@ public class XmlToCsv {
 					}
 				}
 
-				// merge language in table containing all languages
+				// merge current xml table in table containing all languages
 				XmlToCsv.mergeTables(table, langTable);
 
-			} else {
-				System.out.println("Directory "+file.getAbsolutePath()+" has not the right format. Try renaming it like a target language you chose.");
 			}
 		}
 
-		System.out.println(csvFile.getAbsolutePath());
 		for (ArrayList<String> row : table) {
 			writer.writeNext(row.toArray(new String[row.size()]));
 		}
 		writer.close();
+		System.out.println("Done. Wrote string table to "+csvFile);
+	}
+
+	/**
+	 * recursive method looking for res folder 
+	 * @param directory
+	 * @param depth search depth, normally just 1
+	 * @param currentDepth initialize with 0
+	 * @return resource folder named "res" if available or null
+	 */
+	private static File findResourceFolder(File directory, int depth, int currentDepth){
+		File resDirectory = null;
+		
+		if(currentDepth > depth || !directory.isDirectory() || directory.getName().equals("bin")){
+			resDirectory = null;
+		} else {
+			if(directory.getName().equals("res")){
+				resDirectory =  directory;
+			} else {
+				
+				for (File child : directory.listFiles()) {
+					resDirectory = findResourceFolder(child, depth, currentDepth + 1);
+					if(resDirectory != null && resDirectory.getName().equals("res")){
+						return resDirectory;
+					}
+				}
+			}
+		}
+		return resDirectory;
 	}
 
 	private static void addHeaderToCsv(CSVWriter writer, String origLanguage, String[] targetLanguages){
@@ -95,26 +141,52 @@ public class XmlToCsv {
 		}
 	}
 
-	private static File createCsvDirectory(String path, String fileName){
-		File newDir = new File(path + "csv/");
-		newDir.mkdir();
-
-		String tmp = newDir.getAbsolutePath() + "/" + ((fileName != null) ? fileName : "androidStringResources") +".csv";
-		return new File(tmp);
+	/**
+	 * Create csv file and all necessary parent directories
+	 * @param path absolute path to csv file including file name
+	 * @return csv file
+	 */
+	private static File createCsvFile(String path){
+		if(path == null || path.equals("")){
+			path = "androidStringResources.csv";
+		}
+		
+		File csvFile = new File(path);
+		File parentFile = csvFile.getParentFile();
+		
+		if(!parentFile.exists() && !parentFile.mkdirs()){
+		    return null;
+		}
+		return csvFile;
 	}
 
-	private static boolean equalsSpecifiedLanguage(String name, String origLanguage, String[] targetLanguages){
-		boolean result = false;
-		for (String string : targetLanguages) {
-			if(name.equals(string)) result = true;
+	/**
+	 * Check if the current folder is a values folder with the matching locale
+	 * @param name name of directory
+	 * @param origLanguage
+	 * @param targetLanguages
+	 * @return
+	 */
+	private static boolean isLanguageFolder(String name, String origLanguage, String[] targetLanguages){
+		if(!name.contains("values")){
+			return false;
+		} else {
+//			for (String string : targetLanguages) {
+//				if(name.equals("values-" + string)){
+//					return true;
+//				}
+//			}
+			if(name.equals("values-" + origLanguage)){
+				return true;
+			}
 		}
-		if(origLanguage.equals(name)) result = true;
-
-		return result;
+		return false;
 	}
 	
-	public static ArrayList<ArrayList<String>> readXmlStrings(Node current, ArrayList<ArrayList<String>> table, int coloumns, String fileName) {
+	public static ArrayList<ArrayList<String>> readXmlFile(Node current, ArrayList<ArrayList<String>> table, int coloumns, String fileName) {
 		for (int i = 0; i < current.getChildCount(); i++) {
+			
+			// pair of string id and string value
 			String[] pair = readXmlElement((current.getChild(i)));
 			if(pair != null){
 				ArrayList<String> row = new ArrayList<String>(coloumns);
@@ -141,6 +213,12 @@ public class XmlToCsv {
 					resourceValue != null &&
 					!resourceValue.startsWith("@string/") &&
 					!resourceValue.startsWith("@array/")){
+					
+					if(resourceValue.contains("\n")){
+						System.out.println("[WARNING]: String resource " + resourceName + "contains line breaks." +
+								" Generated csv file might be invalid!");
+					}
+					
 					return new String[]{resourceName, resourceValue};
 				}
 			}
